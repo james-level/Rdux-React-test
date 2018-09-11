77f506740c4443038c6e31a4c6c979113c4c7404
James Henderson <hendersonjamesalexander@gmail.com>
2018-09-11 23:13:59 +0100

    creates App.js at base of components directory

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const App = (props => {
+  return (
+      <div className="container">
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <a className="navbar-brand" href="#">Reactive Keys</a>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About Us</Link></li>
+                <li><Link to="/instruments">Product Range</Link></li>
+                <li><Link to="/cart">Basket</Link></li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        {/* Each smaller components */}
+        {props.children}
+      </div>
+    );
+  };
+
+export default App;
