c1ae03f2b90e5ae5e3b2706867dcdbdfb2007851
James Henderson <hendersonjamesalexander@gmail.com>
2018-09-11 22:48:49 +0100

    creates home page for shop

---
+++ src/components/common/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Reactive Keys</h1>
+      <p>Here you'll find an array of keyed instruments to meet all your ivory-tinkling needs.</p>
+    </div>
+  )
+}
+
+export default Home;
