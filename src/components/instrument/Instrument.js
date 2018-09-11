e88d946d7324ea4436a2c913cc75d7310491ec0e
James Henderson <hendersonjamesalexander@gmail.com>
2018-09-11 22:55:11 +0100

    creates instrument constructor and submitInstrument method

--- src/components/instrument/Instrument.js
+++ src/components/instrument/Instrument.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+class Instrument extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  submitInstrument(input){
+    alert('Submitted.')
+  }
+
+  render(){}
+}
