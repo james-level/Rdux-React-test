5782511607043718de85c6f8c12f2dd919d5794a
James Henderson <hendersonjamesalexander@gmail.com>
2018-09-11 23:04:26 +0100

    adds render function, partially complete

--- src/components/instrument/Instrument.js
+++ src/components/instrument/Instrument.js
@@ -9,5 +9,32 @@ class Instrument extends React.Component{
     alert('Submitted.')
   }
 
-  render(){}
+  render(){
+
+    let nameInput;
+    let descriptionInput;
+    let priceInput;
+    let quantityInput;
+    let imageInput;
+
+    return(
+      <div>
+        <h3>Current Product Range</h3>
+        <ul>
+          {/* Go through instruments array */ }
+          {this.props.instruments.map((instrument, index) => <li key={index}>{instrument.name}</li>
+         )}
+        </ul>
+        <div>
+          <h3>Instrument Form</h3>
+          <form onSubmit={e => {
+            e.preventDefault();
+            var input = {name: nameInput.value, description: descriptionInput.value, price: priceInput.value, quantity: quantityInput.value, image: imageInput.value}
+          }}
+        </div>
+      </div>
+    )
+  }
 }
+
+export default Instrument;
